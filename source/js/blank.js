532aa6e5a014c6a84461551ed6c0cca3c1709d46
etpotato <mareev.pv@gmail.com>
2021-05-05 15:04:57 +0300

    First commit on new type of gh-pages

--- source/js/blank.js
+++
@@ -1,8 +0,0 @@
-const backLink = document.querySelector('.blank__link');
-
-const onBackClick = (evt) => {
-  evt.preventDefault();
-  window.history.back();
-};
-
-backLink.addEventListener('click', onBackClick);
--- source/js/util.js
+++
@@ -1,7 +0,0 @@
-const escKeyValues = ['Escape', 'Esc'];
-
-const isEsc = (evt) => {
-  return escKeyValues.some((code) => evt.key === code);
-};
-
-export { isEsc };
